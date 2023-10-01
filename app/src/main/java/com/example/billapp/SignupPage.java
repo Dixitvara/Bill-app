9ece161f459b2bcf7a4597c1cf04c93f735ee4bf
dixit <varadixit@gmail.com>
2023-10-01 22:07:14 +0530

    signup page created

--- app/src/main/java/com/example/billapp/SignupPage.java
+++ app/src/main/java/com/example/billapp/SignupPage.java
@@ -1,18 +1,15 @@
 package com.example.billapp;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-//package actionbar;
-public class MainActivity extends AppCompatActivity {
+
+public class SignupPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup_page);
 
         // action bar
         new actionBar().getBar();
